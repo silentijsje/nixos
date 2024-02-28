e7f16a9aa04dfa14f18a29ce4430127557b4d0d3
Stanley <github@silentijsje.com>
2024-02-28 16:36:46 +0100

    Update options.nix

--- options.nix
+++ options.nix
@@ -4,10 +4,10 @@
 
 let
   # THINGS YOU NEED TO CHANGE
-  username = "zaney";
-  hostname = "hyprnix";
+  username = "gamer0308";
+  hostname = "nixos";
   userHome = "/home/${username}";
-  flakeDir = "${userHome}/zaneyos";
+  flakeDir = "${userHome}/flakes";
   waybarStyle = "slickbar-num"; # simplebar, slickbar, slickbar-num, or default
 in {
   # User Variables
@@ -28,51 +28,10 @@ in {
   flakeDir = "${flakeDir}";
   flakePrev = "${userHome}/.zaneyos-previous";
   flakeBackup = "${userHome}/.zaneyos-backup";
-  terminal = "alacritty"; # This sets the terminal that is used by the hyprland terminal keybinding
-
-  # System Settings
-  clock24h = false;
-  theLocale = "en_US.UTF-8";
-  theKBDLayout = "us";
-  theKBDVariant = "";
-  theLCVariables = "en_US.UTF-8";
-  theTimezone = "Europe/Amsterdam";
-  theShell = "bash"; # Possible options: bash, zsh
-  theKernel = "default"; # Possible options: default, latest, lqx, xanmod, zen
-  sdl-videodriver = "x11"; # Either x11 or wayland ONLY. Games might require x11 set here
-  # For Hybrid Systems intel-nvidia
-  # Should Be Used As gpuType
-  cpuType = "intel";
 
   # Enable / Setup NFS
   nfs = false;
   nfsMountPoint = "/mnt/nas";
   nfsDevice = "nas:/volume1/nas";
-
-  # NTP & HWClock Settings
-  ntp = true;
-  localHWClock = false;
-
-  # Enable Printer & Scanner Support
-  printer = false;
-
-  # Enable Flatpak & Larger Programs
-  distrobox = false;
-  flatpak = false;
-  kdenlive = true;
-  blender = true;
-
-  # Enable Support For
-  # Logitech Devices
-  logitech = true;
-
-  # Enable Terminals
-  # If You Disable All You Get Kitty
-  wezterm = false;
-  alacritty = true;
-  kitty = false;
-
-  # Enable Python & PyCharm
-  python = false;
 
 }
