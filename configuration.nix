ee2e355e1ebb168f299c8784ee9746232b91f0ff
Stanley <github@silentijsje.com>
2024-06-26 16:11:00 +0200

    chore: Refactor OCI container configuration for Home Assistant with Podman

--- configuration.nix
+++ configuration.nix
@@ -95,17 +95,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
-
-  virtualisation.oci-containers = {
-    backend = "podman";
-    containers.homeassistant = {
-      volumes = [ "home-assistant:/config" ];
-      environment.TZ = "Europe/Amsterdam";
-      image = "ghcr.io/home-assistant/home-assistant:stable"; # Warning: if the tag does not change, the image will not be updated
-      extraOptions = [ 
-        "--network=host" 
-        "--device=/dev/ttyACM0:/dev/ttyACM0"  # Example, change this to match your own hardware
-      ];
-    };
-  };
 }
