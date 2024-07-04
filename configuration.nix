8a929a85e844660c4e20d59c3085cc704b51c397
Stanley <github@silentijsje.com>
2024-07-04 18:43:44 +0200

    chore: Update networking.firewall.allowedTCPPorts in configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -97,10 +97,10 @@ users.users.stanley.openssh.authorizedKeys.keys = [
   services.openssh.enable = true;
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 80 443 22 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
