fee999a0c60a40097c11c9200af5ef529c2f4af4
Stanley <github@silentijsje.com>
2024-07-04 18:33:39 +0200

    chore: Update configuration.nix with various changes
    - Update bootloader configuration to use GRUB instead of systemd-boot
    - Update hardware-configuration.nix to use /etc/nixos/hardware-configuration.nix
    - Update openssh authorized keys for user "stanley"
    - Update openssh authorized keys in configuration.nix
    - Update Docker configuration in configuration.nix
    - Fix syntax error in configuration.nix
    - Enable Docker rootless mode with socket variable set
    - Update X11 keymap configuration in configuration.nix
    - Update install-os.sh permissions

--- configuration.nix
+++ configuration.nix
@@ -7,7 +7,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      /etc/nixos/hardware-configuration.nix
+      ./hardware-configuration.nix
     ];
 
   # Bootloader.
@@ -44,9 +44,9 @@
   };
 
   # Configure keymap in X11
-  services.xserver.xkb = {
+  services.xserver = {
     layout = "us";
-    variant = "";
+    xkbVariant = "";
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
@@ -57,31 +57,17 @@
     packages = with pkgs; [];
   };
 
-users.users.stanley.openssh.authorizedKeys.keys = [
-  # "ssh-rsa AAAAB3Nz....6OWM= user" # content of authorized_keys file
-  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPC6PrQT7ULeTRj+4WPabG4tFeRoS8Po9KeThRyjTjnP prod-code02"
-  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHSLCbslrptciBVTmU8iaBDldsZ7QCPdtmAIiFNO+rqb Stanley-PC-Putty"
-  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBXqJaNm9Qbk9Y76a7+UN9AV4MxasmXc2w+QgOkbCt1b stanley-laptop"
-];
-
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    wget
-    git
-    docker
-    docker-compose
-    bash
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  wget
+  bash
+  git
   ];
-
-  virtualisation.docker.enable = true;
-  virtualisation.docker.rootless = {
-    enable = true;
-    setSocketVariable = true;
-  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -97,10 +83,10 @@ users.users.stanley.openssh.authorizedKeys.keys = [
   services.openssh.enable = true;
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 80 443 22 8123 ];
+  # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  networking.firewall.enable = false;
+  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -108,5 +94,6 @@ users.users.stanley.openssh.authorizedKeys.keys = [
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
+  system.stateVersion = "23.11"; # Did you read the comment?
+
 }
