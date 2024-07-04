f89384de525194f46789b08e86586f2625caade6
Stanley <github@silentijsje.com>
2024-07-04 18:48:50 +0200

    git fetch and deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+cd ../
+git pull
+build_version=$(git rev-parse --short HEAD)
+echo "$(TZ='Europe/Amsterdam' date +%FT%T%z): Releasing new server version. $build_version" >> #/mnt/backup/code/github/logs/prod-code02.log
+echo "$(TZ='Europe/Amsterdam' date +%FT%T%z): Running build...."
+./install.sh
---
+++ scripts/fetchgit.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Define an array of directories
+directories=(
+    "../"
+)
+
+# Iterate over each directory
+for dir in "${directories[@]}"; do
+    echo "Processing directory: $dir"
+    cd "$dir" || exit
+
+    # Fetch the remote repository
+    echo "$(TZ='Europe/Amsterdam' date +%FT%T%z): Fetching remote repository..."
+    git fetch
+
+    # Set the upstream branch
+    UPSTREAM=${1:-'@{u}'}
+
+    # Get the local, remote, and base commit hashes
+    LOCAL=$(git rev-parse @)
+    REMOTE=$(git rev-parse "$UPSTREAM")
+    BASE=$(git merge-base @ "$UPSTREAM")
+
+    # Check if the local and remote branches are the same
+    if [ "$LOCAL" = "$REMOTE" ]; then
+        echo "$(TZ='Europe/Amsterdam' date +%FT%T%z): Up-to-date"
+    elif [ "$LOCAL" = "$BASE" ]; then
+        BUILD_VERSION=$(git rev-list HEAD)
+        echo "$(TZ='Europe/Amsterdam' date +%FT%T%z): Changes detected, deploying new version $BUILD_VERSION"
+        ./scripts/deploy.sh
+    elif [ "$REMOTE" = "$BASE" ]; then
+        echo "$(TZ='Europe/Amsterdam' date +%FT%T%z): Local changes detected, stashing..."
+        git stash
+        ./scripts/deploy.sh
+    else
+        echo "$(TZ='Europe/Amsterdam' date +%FT%T%z): Diverged"
+    fi
+done
