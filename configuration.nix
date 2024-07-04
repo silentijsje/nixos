08aef0888e9d654b36dda4e1014cd2ba4f0e2d24
Stanley <github@silentijsje.com>
2024-07-04 18:47:12 +0200

    chore: Update networking.hostName in configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -15,7 +15,7 @@
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "prod-nix-media02"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
